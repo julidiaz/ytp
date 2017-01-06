8c2d138960e7ec42295335dd5fec4da9fff5b4a3
juliancoder-imac <juliancoder@gmail.com>
2017-01-06 22:47:26 +0100

    User class created. Template for AppComponent created.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
 
+// Here we can define other classes used in the app, if needed ...
+// We need to include the "export" keyword to make the class available outside this component
+export class User {
+	id: number;
+	fullName: string;
+	login: string;
+	password: string
+}
+
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	moduleId: module.id,
+  	selector: 'my-app',
+  	templateUrl: 'app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	titleApp: string 		= 'YTP';
+	versionApp: string		= 'Development Version 1.0';
+
+	// We create an initial user
+	oneUser: User = {
+		id: 1,
+		fullName: 'The WebMaster',
+		login: 'juliancoder',
+		password: 'coder2020'	
+	};
+}
