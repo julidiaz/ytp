7b91e71334138ae72f67e50ea8bf883b29388657
juliancoder-imac <juliancoder@gmail.com>
2017-01-07 12:44:08 +0100

    Adding two-way data-binding. Necessary to import FormsModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
+import { NgModule }      	from '@angular/core';
+import { BrowserModule } 	from '@angular/platform-browser';
+// We import FormsModule to allow two-way data binding
+// in forms fields
+import { FormsModule }		from '@angular/forms';
+import { AppComponent }  	from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+	// We have also need to add FormsModule here ...
+	imports:      [ BrowserModule, FormsModule ],
+  	declarations: [ AppComponent ],
+  	bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
