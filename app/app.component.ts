e2711807e6db45f16a99f0ea6e1d923b047e2841
juliancoder-imac <juliancoder@gmail.com>
2017-01-12 21:17:54 +0100

    Added list of users info, *ngFor and *ngIf directives and master-detail contents in template, with event handler for user selected in list (click event)

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,6 +13,17 @@ export class User {
 	password: string;
 }
 
+// We create an array of users
+const USERS: User[] = [
+	{ id: 1, fullName: 'Patrick Everet', email: 'patrick@user.com', mobile: '600123456' },
+	{ id: 2, fullName: 'Mike Fesch', email: 'mike@user.com', mobile: '655666999' },
+	{ id: 3, fullName: 'Paul Cosman', email: 'paul@user.com', mobile: '678222999' },
+	{ id: 4, fullName: 'Billy Ramirez', email: 'billy@user.com', mobile: '699222333' },
+	{ id: 5, fullName: 'Sam Vastin', email: 'sam@user.com', mobile: '611222999' },
+	{ id: 6, fullName: 'Peter Morris', email: 'peter@user.com', mobile: '655888999' },
+	{ id: 7, fullName: 'Carl Maluna', email: 'carl@user.com', mobile: '699000222' }
+];
+
 @Component({
 	moduleId: module.id,
   	selector: 'my-app',
@@ -32,8 +43,20 @@ export class AppComponent  {
 	// For testing purpose: we want to test the ngClass directive
 	isUserCredentials: boolean	= false;
 
-	// We create an initial user
-	oneUser: User = {
+	// List of users: we initialize the component's list of users with the 
+	// array we created previously; by this property ("listOfUsers") we made accesible 
+	// the users info. from the component to the template to show it
+	listOfUsers = USERS;
+	
+	// We create an initial user ("oneUser"), of type "User"
+	// UPDATE: now we don't need anymore this static user;
+	// Instead of that, we create a "selectedHero" property to manage and expose
+	// from the component the information of the User object that is selected in template
+	// We've decided that none of the users should be selected by default before the
+	// users picks one of the list, so we don't initialize the "selectedHero" (as we were doing with the static "oneUser" variable)
+	selectedUser: User;
+	
+	/*oneUser: User = {
 		id: 1,
 		fullName: 'The WebMaster',
 		email: 'juliancoder@gmail.com',
@@ -42,7 +65,8 @@ export class AppComponent  {
 		country: 'Spain',
 		login: 'juliancoder',
 		password: 'coder2020',	
-	};
+	};*/
+	
 
 	// Methods () ---------------------------
 
@@ -74,5 +98,13 @@ export class AppComponent  {
 		};
 
 		return cssInlineStyles;
+	}
+
+	// onSelect(user): event handler, fired up in component template (app.component.html) when
+	// click over an user in the list
+	onSelect(user: User): void {
+		// We set our "selectedUser" property in component
+		// with the user that has been clicked in the template ("user" argument)
+		this.selectedUser = user;
 	}
 }
