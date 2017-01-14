7c327d84e36963edfed3718bc3811ab5db99efd1
juliancoder-imac <juliancoder@gmail.com>
2017-01-14 11:23:17 +0100

    Fixing appComponent.ts and appComponent.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,13 +15,13 @@ export class User {
 
 // We create an array of users
 const USERS: User[] = [
-	{ id: 1, fullName: 'Patrick Everet', email: 'patrick@user.com', mobile: '600123456' },
-	{ id: 2, fullName: 'Mike Fesch', email: 'mike@user.com', mobile: '655666999' },
-	{ id: 3, fullName: 'Paul Cosman', email: 'paul@user.com', mobile: '678222999' },
-	{ id: 4, fullName: 'Billy Ramirez', email: 'billy@user.com', mobile: '699222333' },
-	{ id: 5, fullName: 'Sam Vastin', email: 'sam@user.com', mobile: '611222999' },
-	{ id: 6, fullName: 'Peter Morris', email: 'peter@user.com', mobile: '655888999' },
-	{ id: 7, fullName: 'Carl Maluna', email: 'carl@user.com', mobile: '699000222' }
+	{ id: 1, fullName: 'Patrick Everet', email: 'patrick@user.com', mobile: '600123456', city: 'Cadiz', country: 'Spain', login: 'patriko', password: '123' },
+	{ id: 2, fullName: 'Mike Fesch', email: 'mike@user.com', mobile: '655666999', city: 'Seville', country: 'Spain', login: 'miki', password: '456' },
+	{ id: 3, fullName: 'Paul Cosman', email: 'paul@user.com', mobile: '678222999', city: 'Malaga', country: 'Spain', login: 'paulino', password: '789' },
+	{ id: 4, fullName: 'Billy Ramirez', email: 'billy@user.com', mobile: '699222333', city: 'Jaen', country: 'Spain', login: 'billiam', password: '000' },
+	{ id: 5, fullName: 'Sam Vastin', email: 'sam@user.com', mobile: '611222999', city: 'Huelva', country: 'Spain', login: 'samuel', password: '111' },
+	{ id: 6, fullName: 'Peter Morris', email: 'peter@user.com', mobile: '655888999', city: 'Almeria', country: 'Spain', login: 'pedrito', password: '222' },
+	{ id: 7, fullName: 'Carl Maluna', email: 'carl@user.com', mobile: '699000222', city: 'Granada', country: 'Spain', login: 'carlitos', password: '333' }
 ];
 
 @Component({
